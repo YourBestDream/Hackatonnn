7d082c7c92e0eb338efb576645287779fc1e956e
YourBestDream <ferari100w3@gmail.com>
2023-11-19 05:35:13 +0200

    Changed the logo in favicon

--- src/components/ModalQuestions.js
+++ src/components/ModalQuestions.js
@@ -1,43 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
 import { Modal, Form, Button } from '@themesberg/react-bootstrap';
 
 const ModalQuestions = ({ showModal, handleCloseModal, selectedMeeting, handleSelectMeeting, meetingOptions }) => {
-  return (
-    <Modal show={showModal} onHide={handleCloseModal}>
-      <Modal.Header closeButton>
-        <Modal.Title>New Question</Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        <Form>
-          <Form.Group className="mb-3">
-            <Form.Label>Title</Form.Label>
-            <Form.Control type="text" placeholder="Enter title" />
-          </Form.Group>
-          <Form.Group className="mb-3">
-            <Form.Label>Description</Form.Label>
-            <Form.Control as="textarea" rows={3} placeholder="Enter the description of your problem" />
-          </Form.Group>
-          <Form.Group className="mb-3">
-            <Form.Label>Select a Meeting</Form.Label>
-            <Form.Select
-              value={selectedMeeting}
-              onChange={(e) => handleSelectMeeting(e.target.value)}
-            >
-              <option value="">Select a Meeting</option>
-              {meetingOptions.map((option, idx) => (
-                <option key={idx} value={option}>{option}</option>
-              ))}
-            </Form.Select>
-          </Form.Group>
-        </Form>
-      </Modal.Body>
-      <Modal.Footer>
-        <Button variant="primary" onClick={handleCloseModal}>
-          Send Question
-        </Button>
-      </Modal.Footer>
-    </Modal>
-  );
+    const [title, setTitle] = useState("");
+
+    const handleTitleChange = (e) => {
+        setTitle(e.target.value);
+    };
+
+    const isDisabled = !title || !selectedMeeting;
+
+    return (
+        <Modal show={showModal} onHide={handleCloseModal}>
+            <Modal.Header closeButton>
+                <Modal.Title>New Question</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <Form>
+                    <Form.Group className="mb-3">
+                        <Form.Label>Title</Form.Label>
+                        <Form.Control 
+                            type="text" 
+                            placeholder="Enter title" 
+                            value={title} 
+                            onChange={handleTitleChange} 
+                        />
+                    </Form.Group>
+                    <Form.Group className="mb-3">
+                        <Form.Label>Description</Form.Label>
+                        <Form.Control as="textarea" rows={3} placeholder="Enter the description of your problem" />
+                    </Form.Group>
+                    <Form.Group className="mb-3">
+                        <Form.Label>Select a Meeting</Form.Label>
+                        <Form.Select
+                            value={selectedMeeting}
+                            onChange={(e) => handleSelectMeeting(e.target.value)}
+                        >
+                            <option value="">Select a Meeting</option>
+                            {meetingOptions.map((option, idx) => (
+                                <option key={idx} value={option}>{option}</option>
+                            ))}
+                        </Form.Select>
+                    </Form.Group>
+                </Form>
+            </Modal.Body>
+            <Modal.Footer>
+                <Button 
+                    variant="primary" 
+                    onClick={handleCloseModal}
+                    disabled={isDisabled}
+                >
+                    Send Question
+                </Button>
+            </Modal.Footer>
+        </Modal>
+    );
 };
 
 export default ModalQuestions;
