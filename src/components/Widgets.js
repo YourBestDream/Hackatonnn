0d371e48539dc1a7828caba88ab5f39e7f46cf66
auperman-lab <naauperman@gmail.com>
2023-11-19 03:55:21 +0200

    changed settings and some widgets , also added a role based field for position

--- src/components/Widgets.js
+++ src/components/Widgets.js
@@ -12,51 +12,35 @@ import ProfileCover from "../assets/img/profile-cover.jpg";
 import teamMembers from "../data/teamMembers";
 
 
-export const ProfileCardWidget = () => {
+export const ProfileCardWidget = ({email, phone, name, surname}) => {
   return (
     <Card border="light" className="text-center p-0 mb-4">
       <div style={{ backgroundImage: `url(${ProfileCover})` }} className="profile-cover rounded-top" />
-      <Card.Body className="pb-5">
+      <Card.Body className="pb-4">
         <Card.Img src={Profile1} alt="Neil Portrait" className="user-avatar large-avatar rounded-circle mx-auto mt-n7 mb-4" />
-        <Card.Title>Neil Sims</Card.Title>
-        <Card.Subtitle className="fw-normal">Senior Software Engineer</Card.Subtitle>
-        <Card.Text className="text-gray mb-4">New York, USA</Card.Text>
+        <Card.Title>{name} {surname}</Card.Title>
 
-        <Button variant="primary" size="sm" className="me-2">
-          <FontAwesomeIcon icon={faUserPlus} className="me-1" /> Connect
-        </Button>
-        <Button variant="secondary" size="sm">Send Message</Button>
-      </Card.Body>
-    </Card>
-  );
-};
+        <Card.Text className="text-gray mb-4">Country: Romania, RO</Card.Text>
+        <Card.Text className="text-gray mb-4">Email: {email}</Card.Text>
+        <Card.Text className="text-gray mb-4">Phone Number: {phone}</Card.Text>
 
-export const ChoosePhotoWidget = (props) => {
-  const { title, photo } = props;
+        <h6 className="mb-4">Select profile photo</h6>
 
-  return (
-    <Card border="light" className="bg-white shadow-sm mb-4">
-      <Card.Body>
-        <h5 className="mb-4">{title}</h5>
-        <div className="d-xl-flex align-items-center">
-          <div className="user-avatar xl-avatar">
-            <Image fluid rounded src={photo} />
-          </div>
-          <div className="file-field">
-            <div className="d-flex justify-content-xl-center ms-xl-3">
-              <div className="d-flex">
+        <div className="file-field">
+          <div className="d-flex justify-content-xl-center ms-xl-3">
+            <div className="d-flex">
                 <span className="icon icon-md">
                   <FontAwesomeIcon icon={faPaperclip} className="me-3" />
                 </span>
-                <input type="file" />
-                <div className="d-md-block text-start">
-                  <div className="fw-normal text-dark mb-1">Choose Image</div>
-                  <div className="text-gray small">JPG, GIF or PNG. Max size of 800K</div>
-                </div>
+              <input type="file" />
+              <div className="d-md-block text-start">
+                <div className="fw-normal text-dark mb-1">Choose Image</div>
+                <div className="text-gray small">JPG, GIF or PNG. Max size of 800K</div>
               </div>
             </div>
           </div>
         </div>
+
       </Card.Body>
     </Card>
   );
